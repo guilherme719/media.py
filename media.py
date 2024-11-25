8cb8b2cb14375dba71631c3998302999a15c401c
guilherme719 <guilhermehenriquezica7@gmail.com>
2024-11-25 08:56:03 -0300

    atualizado telas de boas vindas

--- media.py
+++ media.py
@@ -1,14 +1,17 @@
- notas = []
+print("Bem Vindo ao Sistema de Cadastro de Notas")
+
+
+notas = []
 
 for i in range(1, 5):
     nota = float(input(f" nota {i}: "))
     notas.append(nota)
 
- media = sum(notas) / len(notas)
+media = sum(notas) / len(notas)
 
- print(f"A média das notas é: {media:.2f}")
+print(f"A média das notas é: {media:.2f}")
 
- if media >= 6:
+if media >= 6:
     print("***Aprovado parabens!!!!!*****")
 else:
     print("____Reprovado seu burro____ .!.")
